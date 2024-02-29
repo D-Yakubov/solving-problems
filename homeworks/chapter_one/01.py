369b51910bfbfd4ce8d65607ff4b67f98c130118
D-Yakubov <yakubovdastonbek@gmail.com>
2024-02-29 12:13:11 +0500

    added homeworks

---
+++ homeworks/chapter_one/01.py
@@ -0,0 +1,6 @@
+#Problem #1 DMOJ problem wc16c1j1, A Spooky Season
+
+num = int(input())
+
+result = "sp"+"o"*num+"ky"
+print(result)
---
+++ homeworks/chapter_one/02.py
@@ -0,0 +1,10 @@
+#2 DMOJ problem wc15c2j1, A New Hope
+
+num = int(input())
+word = "far, "
+#comma = ","
+sentence = "A long time ago in a galaxy " + (word)*num + "away..."
+
+print(sentence.rstrip(","))
+
+
